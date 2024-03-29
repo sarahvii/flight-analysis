7fbceaa879d1fd741ebebade3c45d0c91cd271ab
Sarah Smith <sarahsmithvii@gmail.com>
2024-03-29 23:48:46 +0000

    created api/full-flight-details endpoint

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
+const {
+  combineFlightsAndSegments,
+} = require("./utils/combineFlightsAndSegments");
+const { readCSV } = require("./utils/csvReader");
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.get("/api/full-flight-details", async (req, res) => {
+  try {
+    const flights = await readCSV("./data/flighdata_B.csv");
+    const segments = await readCSV("./data/flighdata_B_segments.csv");
+    const fullFlightDetails = combineFlightsAndSegments(flights, segments);
+    res.json(fullFlightDetails);
+  } catch (error) {
+    console.error("Failed to fetch full flight details: ", error);
+    res.status(500).send("Error fetching full flight details");
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${3000}`);
+  console.log(`Server listening on port ${3000}`);
 });
